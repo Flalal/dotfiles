2a1f81dfde5deb652290610525852826de0fe9c5
Florian Flahaut <fflahaut@ff-corp.fr>
2025-06-13 00:14:04 +0200

    chore: gitconfig added

--- exportConfig.sh
+++ exportConfig.sh
@@ -6,5 +6,6 @@ cp -r config/hyprland/waybar ~/.config/
 cp -r config/hyprland/rofi ~/.config/
 cp -r config/hyprland/hypr ~/.config/
 cp -r config/hyprland/nvim ~/.config/
+cp config/git/.gitconfig ~/.gitconfig
 echo "Les configurations ont été exportées vers ~/.config."
 
--- retreiveConfig.sh
+++ retreiveConfig.sh
@@ -5,6 +5,6 @@ cp -r ~/.config/waybar config/hyprland/
 cp -r ~/.config/rofi config/hyprland/
 cp -r ~/.config/hypr config/hyprland/
 cp -r ~/.config/nvim config/hyprland/
-
+cp ~/.gitconfig config/git/
 echo "Les configurations ont été copiées dans le répertoire courant."
 
