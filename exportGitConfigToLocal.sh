aab59c2da81f174f573c6a4811c13bf1c735f2db
Florian Flahaut <fflahaut@ff-corp.fr>
2025-06-13 19:44:27 +0200

    chore: automatic install package

--- exportGitConfigToLocal.sh
+++ exportGitConfigToLocal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Importation des configs"
 # Copier les configurations depuis le répertoire courant vers ~/.config
 cp -r config/hyprland/.config/dunst ~/.config/
 cp -r config/hyprland/.config/waybar ~/.config/
@@ -10,5 +11,11 @@ cp -r config/hyprland/.config/nvim ~/.config/
 cp -r config/kitty ~/.config/
 
 cp -r config/git/.gitconfig ~/.gitconfig
-echo "Les configurations ont été exportées vers ~/.config."
+echo "Importaion des configs OK"
+echo "Installation des packages"
 
+for x in $(cat installed_package_list.txt); do
+  yay -S --needed $x 2>/dev/null | grep -v -e "est à jour -- ignoré" -e " il n’y a rien à faire" -e "il n'y a rien à faire" -e "Sync Explicit"
+done
+
+echo "Installation des packages OK"
